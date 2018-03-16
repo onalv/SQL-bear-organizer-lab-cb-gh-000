a6df4a0bbc3db2aa12e3957655afadcf0081e19d
Omar Nava <omarnavaalvarez@gmail.com>
2018-03-16 20:56:35 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VAUES ("Mr. Chocolate", 12, "M", "Blue", "Happy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 12, "M", "Blue", "Happy", 0);
